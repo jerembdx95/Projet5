8e54ba4b1473b94d6a7d9ece43523f8af113e8da
Jérémy Meunier <jeremy.mnr1@gmail.com>
2020-06-11 22:37:18 +0200

    update

--- Frontend/panier.js
+++ Frontend/panier.js
@@ -91,120 +91,29 @@ produit.forEach(element => {
  
 /* formulaire */
 
-
-var submit = document.getElementById ("button_form");
-submit.onclick = function(){ 
-
-
-checkInput = () => {
-  //Controle Regex
-  let checkNumber = /[0-9]/;
-  let checkMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let checkSpecialCharacter = /[§!@#$%^&*().?":{}|<>]/;
-
-  //message fin de controle
-  let checkMessage = "";
-
-  //Récupération des inputs
-
   let nom = document.getElementById("nom").value;
   let prenom = document.getElementById("prenom").value;
   let email = document.getElementById("email").value;
   let adresse = document.getElementById("adresse").value;
   let ville = document.getElementById("ville").value;
+  let submit = document.getElementById("button_form");
 
-  /* Test Nom */
+  submit.addEventListener("click", function(){
 
-  if (
-    checkNumber.test(nom) == true ||
-    checkSpecialCharacter.test(nom) == true ||
-    nom == ""
-  ) {
-    checkMessage = "Veuillez vérifier ou renseigner votre nom";
-  } else {
-    console.log("Nom accepté");
-  }
-  
-  /* Test Prenom */
-
-  if (
-    checkNumber.test(prenom) == true ||
-    checkSpecialCharacter.test(prenom) == true ||
-    prenom == ""
-  ) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre prénom";
-  } else {
-    console.log("Prénom accepté");
-  }
-  /* Test Mail */
-
-  if (checkMail.test(email) == false) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre email";
-  } else {
-    console.log("Adresse mail acceptée");
-  }
-  /* Test adresse */
-
-  if (checkSpecialCharacter.test(adresse) == true || adresse == "") {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre adresse";
-  } else {
-    console.log(" Adresse postale acceptée");
-  }
-  /* Test ville */
-  if (
-    (checkSpecialCharacter.test(ville) == true &&
-      checkNumber.test(ville) == true) ||
-    ville == ""
-  ) {
-    checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre ville";
-  } else {
-    console.log("Ville acceptée");
-  }
- 
-  if (checkMessage != "") {
-    alert("Il est nécessaire de :" + "\n" + checkMessage);
-  }
- 
-  else {
-    contact = {
+   contact = {
       lastName: nom,
       firstName: prenom,
       address: adresse,
       city: ville,
       email: email,
     };
-    return contact;
-  }
-};
+    var xhr = new XMLHttpRequest();
 
-/* Check  panier */
-
-checkPanier = () => {
-  let etatPanier = JSON.parse(localStorage.length);
-  if (etatPanier.length < 1) {
-    alert("Votre panier est vide");
-    return false;
-  } else {
-    console.log("Le panier n'est pas vide");
-    return true;
-  }
-};
-
-/* Envoi API */
-
-if (contact == true && etatPanier == true){ 
-
-var xhr = new XMLHttpRequest();
-
-xhr.open("get", "http://localhost:3000/api/teddies/order", true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.send(contact);
-console.log("Bien envoyé")
-windows.location = "./PageValidation.html"
-}
-
-}
-
+    xhr.open("post", "http://localhost:3000/api/teddies/order", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(contact);
+    console.log("Bien envoyé")
+  })
 
 
 
