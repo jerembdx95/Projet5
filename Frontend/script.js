71ba3edb19a2d41c7713bd7755a6d9c0a97bb80a
Jérémy Meunier <jeremy.mnr1@gmail.com>
2020-06-20 14:11:02 +0200

    update

--- Frontend/script.js
+++ Frontend/script.js
@@ -57,8 +57,8 @@ async function teddies(){
 
       container.onclick = function (){
 
-        var stateObj = { foo: "index.html" };
-        history.pushState(stateObj, "index.html", "index.html?id=" + element.id);
+      
+
       
         var page_Liste_produit = document.querySelector('section')
         page_Liste_produit.innerHTML = '';
